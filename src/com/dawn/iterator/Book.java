6416aeebd7a5a59b66fb4c38bbf6b50104b16774
TheDawn <xdc902@hotmail.com>
2018-05-07 20:35:49 +0800

    移动位置

---
+++ src/com/dawn/iterator/Book.java
@@ -0,0 +1,24 @@
+package com.dawn.iterator;
+
+/**
+ * 生成书的实体
+ *
+ * @Author xdc90
+ * @Date 2018/5/7
+ */
+public class Book {
+
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/com/dawn/iterator/Iterator.java
@@ -0,0 +1,20 @@
+package com.dawn.iterator;
+
+/**
+ * @Author xdc90
+ * @Date 2018/5/7
+ */
+public interface Iterator {
+
+    /**
+     * 判断是否有下一个对象
+     * @return
+     */
+    boolean hasNext();
+
+    /**
+     * 获取下一个对象
+     * @return
+     */
+    Object next();
+}
