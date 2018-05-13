1be63a4cb59796704014ebd1cdfc9ad6439f4acb
TheDawn <xdc902@hotmail.com>
2018-05-14 07:28:36 +0800

    singletone methond end

---
+++ src/com/dawn/singleton/Singleton.java
@@ -0,0 +1,18 @@
+package com.dawn.singleton;
+
+/**
+ * @Author xdc90
+ * @Date 2018/5/14
+ */
+public class Singleton {
+
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        System.out.println("生成一个实例");
+    }
+
+    public static Singleton getInstance(){
+        return singleton;
+    }
+}
---
+++ src/com/dawn/singleton/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Singleton模式
+ * 想确保任何情况下都只有1个实例
+ *
+ * 成员类:
+ * Singleton:只存在一个实例的类
+ * Main:测试类
+ *
+ * 重点: Singleton的类的构造器是私有的,避免从Singleton外部调用构造函数
+ * 生成新的实例
+ *
+ * 待补充的知识点:
+ * TODO
+ * 1.类的加载过程
+ * 2.多线程情况下使用该模式如何确保只生成一个实例
+ *
+ * @Author xdc90
+ * @Date 2018/5/10
+ */
+package com.dawn.singleton;
