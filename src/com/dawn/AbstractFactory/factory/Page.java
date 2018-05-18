55d8da0ee4e593e4005c7426a3976f8ba960075b
TheDawn <xdc902@hotmail.com>
2018-05-18 08:06:43 +0800

    abstract factoyr

---
+++ src/com/dawn/AbstractFactory/factory/Page.java
@@ -0,0 +1,8 @@
+package com.dawn.AbstractFactory.factory;
+
+/**
+ * @Author xdc90
+ * @Date 2018/5/18
+ */
+public class Page {
+}
---
+++ src/com/dawn/AbstractFactory/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * AbstractFactory模式
+ *
+ *
+ *
+ * @Author xdc90
+ * @Date 2018/5/18
+ */
+package com.dawn.AbstractFactory;
